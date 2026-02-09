a5cf1e3b5d98cc9a075c7fd4ba69134e32ba8926
HAQaui <luucathai05@icloud.com>
2026-02-09 22:53:09 +0100

    commit 9c8e5b1f0a2c3d4e5f6a7b8c9d0e1f2g3h4i5j6 Author: HAQaui

--- TicTacSchachCat.java
+++ TicTacSchachCat.java
@@ -1,44 +1,258 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.AbstractAction;
+import javax.swing.AbstractButton;
+import javax.swing.AbstractCellEditor;
+import javax.swing.AbstractListModel;
+import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
+import javax.swing.CellEditor;
+import javax.swing.CellRendererPane;
+import javax.swing.ComboBoxEditor;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultBoundedRangeModel;
+import javax.swing.DefaultButtonModel;
+import javax.swing.DefaultCellEditor;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultDesktopManager;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.DefaultSingleSelectionModel;
+import javax.swing.DesktopManager;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.InputMap;
+import javax.swing.InputVerifier;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JDesktopPane;
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JFileChooser;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JPopupMenu;
+import javax.swing.JProgressBar;
+import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import java.awt.AWTEvent;
+import java.awt.AWTEventMulticaster;
+import java.awt.AWTException;
+import java.awt.AWTPermission;
+import java.awt.ActiveEvent;
+import java.awt.Adjustable;
+import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.BufferCapabilities;
+import java.awt.Button;
+import java.awt.Canvas;
+import java.awt.CardLayout;
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
+import java.awt.CheckboxMenuItem;
+import java.awt.Choice;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.ComponentOrientation;
+import java.awt.Composite;
+import java.awt.CompositeContext;
+import java.awt.Container;
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.Dialog;
+import java.awt.Dimension;
+import java.awt.DisplayMode;
+import java.awt.Event;
+import java.awt.EventQueue;
+import java.awt.FileDialog;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Frame;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.ItemSelectable;
+import java.awt.JobAttributes;
+import java.awt.Label;
+import java.awt.LayoutManager;
+import java.awt.LayoutManager2;
+import java.awt.List;
+import java.awt.MediaTracker;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuComponent;
+import java.awt.MenuContainer;
+import java.awt.MenuItem;
+import java.awt.MenuShortcut;
+import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.PopupMenu;
+import java.awt.PrintGraphics;
+import java.awt.PrintJob;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.Robot;
+import java.awt.ScrollPane;
+import java.awt.ScrollPaneAdjustable;
+import java.awt.Scrollbar;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.SystemColor;
+import java.awt.TexturePaint;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.AWTEventListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+import java.awt.event.ContainerListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.HierarchyBoundsAdapter;
+import java.awt.event.HierarchyBoundsListener;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
+import java.awt.event.InputEvent;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
+import javax.swing.border.AbstractBorder;
 
 public class TicTacSchachCat {
 
+    //RoundedBorder für den startJButton
+    class FilledRoundedBorder extends AbstractBorder {
+    private final int arc;
+    private final Color fill;
+    private final Color line;
+
+    FilledRoundedBorder(int arc, Color fill, Color line) {
+        this.arc  = arc;
+        this.fill = fill;
+        this.line = line;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int w, int h) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        Shape rr = new java.awt.geom.RoundRectangle2D.Double(
+            x, y, w - 1, h - 1, arc, arc
+        );
+
+        g2.setColor(fill);
+        g2.fill(rr);
+
+        if (line != null) {
+            g2.setColor(line);
+            g2.draw(rr);
+        }
+
+        g2.dispose();
+    }
+}
+
+
     TicTacSchachCat() {
-        //Fenster erstellen
+        //Hintergrundbild
+        ImageIcon startBildIcon = new ImageIcon("tictacschachStartbild v1.jpg");
+        JLabel hintergrundJLabel = new JLabel(startBildIcon);
+        hintergrundJLabel.setLayout(new GridBagLayout());
+
+        //startButton erstellen
+        JButton startJButton = new JButton();
+        startJButton.setBorder(new FilledRoundedBorder(50, new Color(190,190,190), Color.BLACK));
+        startJButton.setBorderPainted(true);
+        startJButton.setPreferredSize(new Dimension(250,60));
+        startJButton.setContentAreaFilled(false);
+        startJButton.setOpaque(false);
+        startJButton.setFocusPainted(false);
+
+        //Text für startButton erstellen
+        JLabel startButtonText = new JLabel("START");
+        startButtonText.setForeground(Color.WHITE);
+        startButtonText.setFont(new Font("Montserrat", Font.BOLD, 45));
+
+        //Text zum startButton
+        startJButton.add(startButtonText, BorderLayout.CENTER);
+
+        //Button zum Hintergrund
+        hintergrundJLabel.add(startJButton, new GridBagConstraints());
+
+        //Komponenten zum startPanel
+        JPanel startPanel = new JPanel();
+        startPanel.setLayout(new BorderLayout());
+        //startPanel.add(startJButton);
+
+         //Fenster erstellen
         JFrame fenster = new JFrame("Tic-Tac-Schach");
         fenster.setExtendedState(JFrame.MAXIMIZED_BOTH);
         fenster.setLocationRelativeTo(null);
         fenster.setResizable(false);
         fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //startButtonPanel
-        JPanel startButtonPanel = new JPanel();
-        startButtonPanel.setBackground(new Color(190,190,190));
-
-        //Hintergrundbild
-        ImageIcon startBildIcon = new ImageIcon("tictacschachStartbild v1.jpg");
-        JLabel startBildJLabel = new JLabel(startBildIcon);
-
-        //startButton erstellen
-        JButton startJButton = new JButton();
-        startJButton.setFont(new Font("Arial", Font.BOLD, 20));
-        startJButton.setForeground(Color.WHITE);
-        startJButton.setText("START");
-        startJButton.setBounds(0,300,800,600);
-
-        //startButton zum startButtonPanel
-        startButtonPanel.add(startJButton);
-
-        //Komponenten zum startPanel
-        JPanel startPanel = new JPanel();
-        startPanel.setLayout(new BorderLayout());
-        startPanel.add(startBildJLabel, BorderLayout.CENTER);
-        startPanel.add(startButtonPanel, BorderLayout.SOUTH);
+        fenster.setContentPane(hintergrundJLabel);
 
         //Komponenten zum Fenster hinzufügen
-        fenster.add(startPanel);
-         fenster.setVisible(true);
+        
+        fenster.setVisible(true);
     }
 
 public static void main(String[]args) {
